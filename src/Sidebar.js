48c9a3e4842ae6f16fa6f5397056cc143165da69
Alper Torun <alper.torun@piworks.net>
2023-11-25 11:18:07 +0300

    feat: adds new project

---
+++ src/Sidebar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Sidebar.css'
+import { Avatar } from "@material-ui/core"
+import DonutLargeIcon from "@material-ui/icons/DonutLarge"
+import ChatIcon from "@material-ui/icons/Chat"
+import MoreVertIcon from "@material-ui/icons/MoreVert"
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+
+        </div>
+    );
+}
+export default Sidebar
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCyYIhxnEZyeX8iSti--ObuZCSIMv5A1cY",
+    authDomain: "whats-app-clone-dfb71.firebaseapp.com",
+    projectId: "whats-app-clone-dfb71",
+    storageBucket: "whats-app-clone-dfb71.appspot.com",
+    messagingSenderId: "997948125274",
+    appId: "1:997948125274:web:913e84c565b60d420c6863"
+  };
